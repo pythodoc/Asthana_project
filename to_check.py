c7f458f6f22b9024567365040c5cac7cffbf63e4
Gaurav Nazare <76864369+pythodoc@users.noreply.github.com>
2024-10-16 19:04:27 +0530

    Update to_check.py

--- to_check.py
+++ to_check.py
@@ -1,15 +1,15 @@
 import streamlit as st
-from googletrans import Translator
+from translate import Translator
 
-# Initialize the translator
-translator = Translator()
+# Initialize the translator with the target language (Marathi)
+translator = Translator(to_lang='mr')
 
 # Text to be translated
 status = 'light rain'
 st.write(status)  # Display the original text
 
-# Translate the text from English to Marathi (mr)
-translated = translator.translate(status, src='en', dest='hi')
+# Translate the text
+translate_text = translator.translate(status)
 
 # Display the translated text
-st.write(translated.text)
+st.write(translate_text)
