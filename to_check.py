574d856a8b8ee0c13d18eb04959bfa417787baca
Gaurav Nazare <76864369+pythodoc@users.noreply.github.com>
2024-10-16 18:46:59 +0530

    Update new file to check on streamlit

---
+++ to_check.py
@@ -0,0 +1,6 @@
+from google_trans_new import google_translator
+
+translator = google_translator()
+print(status) #light rain 
+translate_text = translator.translate(status, lang_tgt='mr')
+print(translate_text) #chuva leve
