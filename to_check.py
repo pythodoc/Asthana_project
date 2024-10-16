b7474a0d57b6ec4ce06275a60841a0be421910c8
Gaurav Nazare <76864369+pythodoc@users.noreply.github.com>
2024-10-16 18:54:24 +0530

    Update to_check.py

--- to_check.py
+++ to_check.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from google_trans_new import google_translator
 
-# Initialize the translator
-translator = google_translator()
+try:
+    # Initialize the translator
+    translator = google_translator()
 
-# Text to be translated
-status = 'light rain'
-st.write(status)  # Display the original text
+    # Text to be translated
+    status = 'light rain'
+    st.write(status)  # Display the original text
 
-# Translate the text from English (en) to Marathi (mr)
-translate_text = translator.translate(status, lang_src='en', lang_tgt='mr')
+    # Translate the text from English (en) to Marathi (mr)
+    translate_text = translator.translate(status, lang_src='en', lang_tgt='mr')
 
-# Display the translated text
-st.write(translate_text)
+    # Display the translated text
+    st.write(translate_text)
+except Exception as e:
+    st.write(f"An error occurred: {e}")
