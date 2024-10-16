861596bc39bb4524d44d25e8306558658440075c
Gaurav Nazare <76864369+pythodoc@users.noreply.github.com>
2024-10-16 18:52:30 +0530

    Update to_check.py

--- to_check.py
+++ to_check.py
@@ -1,7 +1,15 @@
 import streamlit as st
 from google_trans_new import google_translator
 
+# Initialize the translator
 translator = google_translator()
-st.write(status) #light rain 
-translate_text = translator.translate(status, lang_tgt='mr')
-st.write(translate_text) #chuva leve
+
+# Text to be translated
+status = 'light rain'
+st.write(status)  # Display the original text
+
+# Translate the text from English (en) to Marathi (mr)
+translate_text = translator.translate(status, lang_src='en', lang_tgt='mr')
+
+# Display the translated text
+st.write(translate_text)
